62eaa1a5f5952a322846f9b6bffeec1d57bcf49c
Alejandro Quisbert <alexandro@autistici.org>
2019-04-15 16:03:09 -0400

    POST anidado configurado

--- apps/pages/models.py
+++ apps/pages/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Categories(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
@@ -16,10 +16,10 @@ class Categories(models.Model):
 
 class Applications(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    confidence = models.PositiveSmallIntegerField()
-    version = models.CharField(max_length=100)
-    icon = models.CharField(max_length=100)
-    web_site = models.CharField(max_length=300)
+    confidence = models.PositiveSmallIntegerField(null=True)
+    version = models.CharField(max_length=100, blank=True, null=True)
+    icon = models.CharField(max_length=100, blank=True)
+    web_site = models.CharField(max_length=300, blank=True)
     categories = models.ManyToManyField(Categories)
 
     def __str__(self):
--- apps/pages/serializers.py
+++ apps/pages/serializers.py
@@ -9,7 +9,11 @@ class CategoriesSerializer(serializers.ModelSerializer):
 
 
 class ApplicationsSerializer(serializers.ModelSerializer):
-    categories = CategoriesSerializer(many=True)
+    categories = CategoriesSerializer(many=True, required=False)
+
+    def save(self, **kwargs):
+        print(kwargs, "*********************")
+        return super(ApplicationsSerializer,self).save(**kwargs)
 
     class Meta:
         model = Applications
@@ -19,6 +23,26 @@ class ApplicationsSerializer(serializers.ModelSerializer):
 class SiteUrlSerializer(serializers.ModelSerializer):
     applications = ApplicationsSerializer(many=True)
     # applications = serializers.HyperlinkedRelatedField(many=True, view_name='site-url-detail')
+
+    def create(self, validated_data):
+        applications_data = validated_data.pop('applications')
+        site_url = SiteUrl.objects.create(**validated_data)
+        for application in applications_data:
+            categories_serializer = CategoriesSerializer(data=application['categories'], many=True)
+            categories_serializer.is_valid()
+            cat = categories_serializer.save()
+            application, created = Applications.objects.get_or_create(
+                name=application['name'],
+                confidence=application['confidence'],
+                version=application['version'],
+                icon=application['icon'],
+                web_site=application['web_site']
+            )
+            for category in cat:
+                application.categories.add(category)
+                application.save()
+            site_url.applications.add(application)
+        return site_url
 
     class Meta:
         model = SiteUrl
